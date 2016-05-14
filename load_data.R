59fb346ccf6e629cf95796174c0d1c66f8e2d82b
dassoumya <soumyaranjandas@outlook.com>
2016-05-13 23:55:09 -0500

    Delete load_data.R

--- load_data.R
+++
@@ -1,9 +0,0 @@
-# Load Data from file
-electComsumpt <- read.table("/Users/soumyadas/Desktop/Coursera/Exploratory Graphs/Review 1/household_power_consumption.txt", sep = ";",colClasses = c(rep("character",9)) , na.strings = "?", header = TRUE)
-
-electComsumpt$Date <- as.Date(electComsumpt$Date, "%d/%m/%Y")
-electComsumpt$Time <- strptime(electComsumpt$Time, "%H:%M:%S")
-electComsumpt <- subset(electComsumpt, Date %in% as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d"))
-
-return(electComsumpt)
-
